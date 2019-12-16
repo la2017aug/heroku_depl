f776429d473bc3499a2b102f82922614873172be
Oleksandr Lavrynenko <55446919+la2017aug@users.noreply.github.com>
2019-12-16 13:31:46 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 port = int(os.environ.get("PORT", 5000))
 @app.route('/')
 def hello_world():
-    return 'Flask Dockerized and deployed to Heroku'
+    return 'Flask Dockerized and deployed to Heroku TEST 2019-12-16'
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0',port=port)
